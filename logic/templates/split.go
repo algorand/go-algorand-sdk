b7b88af8fe6bc2492a87cd7c71c7ddad4adea23c
Evan Richard <evan.richard@algorand.com>
2019-11-13 12:52:54 -0500

    WIP

--- logic/templates/split.go
+++ logic/templates/split.go
@@ -3,12 +3,19 @@ package templates
 import (
 	"crypto/sha512"
 	"encoding/base64"
+	"fmt"
+	"github.com/algorand/go-algorand-sdk/crypto"
+	"github.com/algorand/go-algorand-sdk/transaction"
 	"github.com/algorand/go-algorand-sdk/types"
 )
 
 type Split struct {
-	address string
-	program string
+	address     string
+	program     string
+	ratn        uint64
+	ratd        uint64
+	receiverOne string
+	receiverTwo string
 }
 
 const referenceProgram = "ASAIAQUCAAYHCAkmAyCztwQn0+DycN+vsk+vJWcsoz/b7NDS6i33HOkvTpf+YiC3qUpIgHGWE8/1LPh9SGCalSN7IaITeeWSXbfsS5wsXyC4kBQ38Z8zcwWVAym4S8vpFB/c0XC6R4mnPi9EBADsPDEQIhIxASMMEDIEJBJAABkxCSgSMQcyAxIQMQglEhAxAiEEDRAiQAAuMwAAMwEAEjEJMgMSEDMABykSEDMBByoSEDMACCEFCzMBCCEGCxIQMwAIIQcPEBA="
@@ -25,12 +32,58 @@ func (contract Split) GetProgram() string {
 	return contract.program
 }
 
-//GetSendFundsTransaction returns a group transactions array which transfer funds according to the contract's ratio
-// amount: uint64 number of assets to be transferred
+//GetSendFundsTransaction returns a group transaction array which transfer funds according to the contract's ratio
+// the returned byte array is suitable for passing to SendRawTransaction
+// amount: uint64 number of assets to be transferred total
 // precise: handles rounding error. When False, the amount will be divided as closely as possible but one account will get
 // 			slightly more. When true, returns an error.
-func (contract Split) GetSendFundsTransaction(amount uint64, precise bool) ([]types.Transaction, error) {
-	return nil, nil
+func (contract Split) GetSendFundsTransaction(amount uint64, precise bool, firstRound, lastRound, fee uint64, genesisHash []byte) ([]byte, error) {
+	ratio := contract.ratn / contract.ratd
+	amountForReceiverOne := amount * ratio
+	amountForReceiverTwo := amount * (1 - ratio)
+	remainder := amount - amountForReceiverOne - amountForReceiverTwo
+	if precise && remainder != 0 {
+		return nil, fmt.Errorf("could not precisely divide funds between the two accounts")
+	}
+
+	from := contract.address
+	tx1, err := transaction.MakePaymentTxn(from, contract.receiverOne, fee, amountForReceiverOne, firstRound, lastRound, nil, "", "", genesisHash, [32]byte{})
+	if err != nil {
+		return nil, err
+	}
+	tx2, err := transaction.MakePaymentTxn(from, contract.receiverTwo, fee, amountForReceiverTwo, firstRound, lastRound, nil, "", "", genesisHash, [32]byte{})
+	if err != nil {
+		return nil, err
+	}
+	gid, err := crypto.ComputeGroupID([]types.Transaction{tx1, tx2})
+	if err != nil {
+		return nil, err
+	}
+	tx1.Group = gid
+	tx2.Group = gid
+
+	progBytes, err := base64.StdEncoding.DecodeString(contract.program)
+	if err != nil {
+		return nil, err
+	}
+	logicSig, err := crypto.MakeLogicSig(progBytes, nil, nil, crypto.MultisigAccount{})
+	if err != nil {
+		return nil, err
+	}
+	_, stx1, err := crypto.SignLogicsigTransaction(logicSig, tx1)
+	if err != nil {
+		return nil, err
+	}
+	_, stx2, err := crypto.SignLogicsigTransaction(logicSig, tx2)
+	if err != nil {
+		return nil, err
+	}
+
+	var signedGroup []byte
+	signedGroup = append(signedGroup, stx1...)
+	signedGroup = append(signedGroup, stx2...)
+
+	return signedGroup, err
 }
 
 // MakeSplit splits money sent to some account to two recipients at some ratio.
@@ -68,5 +121,6 @@ func MakeSplit(owner, receiverOne, receiverTwo string, ratn, ratd, expiryRound, 
 	injectedProgram := base64.StdEncoding.EncodeToString(injectedBytes)
 	addressBytes := sha512.Sum512_256(injectedBytes)
 	address := types.Address(addressBytes)
-	return Split{address: address.String(), program: injectedProgram}, err
+	split := Split{address: address.String(), program: injectedProgram, ratn: ratn, ratd: ratd, receiverOne: receiverOne, receiverTwo: receiverTwo}
+	return split, err
 }
