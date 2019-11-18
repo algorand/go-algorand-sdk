0f55132b62e5dde062c1e87c50135490700c1483
Evan Richard <evan.richard@algorand.com>
2019-11-18 14:03:24 -0500

    Format comment

--- templates/hashTimeLockedContract.go
+++ templates/hashTimeLockedContract.go
@@ -11,30 +11,24 @@ type HTLC struct {
 	ContractTemplate
 }
 
-//MakeHTLC allows a user to recieve the Algo prior to a deadline (in terms of a round) by proving a knowledge
-//of a special value or to forfeit the ability to claim, returing it to the payer.
-//This contract is usually used to perform cross-chained atomic swaps
+// MakeHTLC allows a user to recieve the Algo prior to a deadline (in terms of a round) by proving a knowledge
+// of a special value or to forfeit the ability to claim, returing it to the payer.
+// This contract is usually used to perform cross-chained atomic swaps
 //
-//More formally -
-//Algos can be transferred under only two circumstances:
-//1. To owner if hash_function(arg_0) = hash_value
-//2. To owner if txn.FirstValid > expiry_round
-//...
+// More formally -
+// Algos can be transferred under only two circumstances:
+// 1. To owner if hash_function(arg_0) = hash_value
+// 2. To owner if txn.FirstValid > expiry_round
+// ...
 //
 //Parameters
 //----------
-//owner : string
-//an address that can receive the asset after the expiry round
-//receiver: string
-//address to receive Algos
-//hashFunction : string
-//the hash function to be used (must be either sha256 or keccak256)
-//hashImage : string
-//the hash image in base64
-//expiryRound : uint64
-//the round on which the assets can be transferred back to owner
-//maxFee : uint64
-//the maximum fee that can be paid to the network by the account
+// - owner : string an address that can receive the asset after the expiry round
+// - receiver: string address to receive Algos
+// - hashFunction : string the hash function to be used (must be either sha256 or keccak256)
+// - hashImage : string the hash image in base64
+// - expiryRound : uint64 the round on which the assets can be transferred back to owner
+// - maxFee : uint64 the maximum fee that can be paid to the network by the account
 func MakeHTLC(owner, receiver, hashFunction, hashImage string, expiryRound, maxFee uint64) (HTLC, error) {
 	var referenceProgram string
 	if hashFunction == "sha256" {
