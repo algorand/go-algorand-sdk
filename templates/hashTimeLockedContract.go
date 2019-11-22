b5f4e2a208b55b77091271a178b4309cb2ad9d7f
Evan Richard <evan.richard@algorand.com>
2019-11-22 10:12:36 -0500

    Fix typo in comment about htlc

--- templates/hashTimeLockedContract.go
+++ templates/hashTimeLockedContract.go
@@ -18,7 +18,7 @@ type HTLC struct {
 //
 // More formally -
 // Algos can be transferred under only two circumstances:
-// 1. To owner if hash_function(arg_0) = hash_value
+// 1. To receiver if hash_function(arg_0) = hash_value
 // 2. To owner if txn.FirstValid > expiry_round
 // ...
 //
