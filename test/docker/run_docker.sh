eb98c6480394e42f59a0d0868851514cd2f48529
mjiang102628 <mjiang102628@gmail.com>
2020-03-19 14:50:51 -0400

    move cucumber tests into go sdk (#127)

---
+++ test/docker/run_docker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm -rf temp
+rm -rf test/features
+git clone --single-branch --branch templates https://github.com/algorand/algorand-sdk-testing.git temp
+
+cp test/docker/sdk.py temp/docker
+mv temp/features test/features
+
+docker build -t sdk-testing -f test/docker/Dockerfile "$(pwd)"
+
+docker run -it \
+     -v "$(pwd)":/opt/go/src/github.com/algorand/go-algorand-sdk \
+     sdk-testing:latest 
