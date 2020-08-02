2b1f2e47b807d86c93d9177709cc7946a5aabbb4
algonautshant <55754073+algonautshant@users.noreply.github.com>
2020-08-02 13:12:25 -0400

    Implementation of @indexer.applications tests (#178)

--- client/v2/indexer/searchForApplications.go
+++ client/v2/indexer/searchForApplications.go
@@ -20,6 +20,19 @@ func (s *SearchForApplications) ApplicationId(applicationId uint64) *SearchForAp
 	return s
 }
 
+// Limit maximum number of results to return.
+func (s *SearchForApplications) Limit(limit uint64) *SearchForApplications {
+	s.p.Limit = limit
+	return s
+}
+
+// Next the next page of results. Use the next token provided by the previous
+// results.
+func (s *SearchForApplications) Next(next string) *SearchForApplications {
+	s.p.Next = next
+	return s
+}
+
 // Do performs HTTP request
 func (s *SearchForApplications) Do(ctx context.Context,
 	headers ...*common.Header) (response models.ApplicationsResponse, err error) {
