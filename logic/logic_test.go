96873cf7debf2072391d012388b39ba837a0c4d2
Jason Paulos <jasonpaulos@users.noreply.github.com>
2021-03-15 10:41:11 -0400

    Add TEAL 3 support

--- logic/logic_test.go
+++ logic/logic_test.go
@@ -36,7 +36,7 @@ func TestCheckProgram(t *testing.T) {
 
 	// invalid opcode
 	program = []byte{1, 32, 1, 1, 34} // int 1
-	program[4] = 128
+	program[4] = 255
 	args[0] = []byte(strings.Repeat("a", 10))
 	err = CheckProgram(program, args)
 	require.EqualError(t, err, "invalid instruction")
@@ -54,13 +54,18 @@ func TestCheckProgram(t *testing.T) {
 	program = append(program, []byte(strings.Repeat("\x02", 800))...) // append 800x keccak256
 	err = CheckProgram(program, args)
 	require.EqualError(t, err, "program too costly to run")
+}
 
+func TestCheckProgramV2(t *testing.T) {
 	// check TEAL v2 opcodes
 	require.True(t, spec.EvalMaxVersion >= 2)
 	require.True(t, spec.LogicSigVersion >= 2)
+
+	args := make([][]byte, 0)
+
 	// balance
-	program = []byte{0x02, 0x20, 0x01, 0x00, 0x22, 0x60} // int 0; balance
-	err = CheckProgram(program, args)
+	program := []byte{0x02, 0x20, 0x01, 0x00, 0x22, 0x60} // int 0; balance
+	err := CheckProgram(program, args)
 	require.NoError(t, err)
 
 	// app_opted_in
@@ -70,6 +75,34 @@ func TestCheckProgram(t *testing.T) {
 
 	// asset_holding_get
 	program = []byte{0x02, 0x20, 0x01, 0x00, 0x22, 0x70, 0x00} // int 0; int 0; asset_holding_get Balance
+	err = CheckProgram(program, args)
+	require.NoError(t, err)
+}
+
+func TestCheckProgramV3(t *testing.T) {
+	// check TEAL v2 opcodes
+	require.True(t, spec.EvalMaxVersion >= 3)
+	require.True(t, spec.LogicSigVersion >= 3)
+
+	args := make([][]byte, 0)
+
+	// min_balance
+	program := []byte{0x03, 0x20, 0x01, 0x00, 0x22, 0x78} // int 0; min_balance
+	err := CheckProgram(program, args)
+	require.NoError(t, err)
+
+	// pushbytes
+	program = []byte{0x03, 0x20, 0x01, 0x00, 0x22, 0x80, 0x02, 0x68, 0x69, 0x48} // int 0; pushbytes "hi"; pop
+	err = CheckProgram(program, args)
+	require.NoError(t, err)
+
+	// pushint
+	program = []byte{0x03, 0x20, 0x01, 0x00, 0x22, 0x81, 0x01, 0x48} // int 0; pushint 1; pop
+	err = CheckProgram(program, args)
+	require.NoError(t, err)
+
+	// swap
+	program = []byte{0x03, 0x20, 0x02, 0x00, 0x01, 0x22, 0x23, 0x4c, 0x48} // int 0; int 1; swap; pop
 	err = CheckProgram(program, args)
 	require.NoError(t, err)
 }
