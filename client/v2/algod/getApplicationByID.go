be02cd30220cb85321af3a314f619b677e56609b
Will Winder <wwinder.unh@gmail.com>
2021-03-19 10:30:38 -0400

    App/Asset by ID

--- client/v2/algod/getApplicationByID.go
+++ client/v2/algod/getApplicationByID.go
@@ -8,18 +8,13 @@ import (
 	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
 )
 
-// GetApplicationByID /v2/applications/{application-id}
-// Given a application id, it returns application information including creator,
-// approval and clear programs, global and local schemas, and global state.
 type GetApplicationByID struct {
-	c             *Client
+	c *Client
+
 	applicationId uint64
 }
 
-// Do performs HTTP request
-func (s *GetApplicationByID) Do(ctx context.Context,
-	headers ...*common.Header) (response models.Application, err error) {
-	err = s.c.get(ctx, &response,
-		fmt.Sprintf("/v2/applications/%d", s.applicationId), nil, headers)
+func (s *GetApplicationByID) Do(ctx context.Context, headers ...*common.Header) (response models.Application, err error) {
+	err = s.c.get(ctx, &response, fmt.Sprintf("/v2/applications/%v", s.applicationId), nil, headers)
 	return
 }
--- client/v2/algod/getAssetByID.go
+++ client/v2/algod/getAssetByID.go
@@ -8,18 +8,13 @@ import (
 	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
 )
 
-// GetAssetByID /v2/assets/{asset-id}
-// Given a asset id, it returns asset information including creator, name, total
-// supply and special addresses.
 type GetAssetByID struct {
-	c       *Client
+	c *Client
+
 	assetId uint64
 }
 
-// Do performs HTTP request
-func (s *GetAssetByID) Do(ctx context.Context,
-	headers ...*common.Header) (response models.Asset, err error) {
-	err = s.c.get(ctx, &response,
-		fmt.Sprintf("/v2/assets/%d", s.assetId), nil, headers)
+func (s *GetAssetByID) Do(ctx context.Context, headers ...*common.Header) (response models.Asset, err error) {
+	err = s.c.get(ctx, &response, fmt.Sprintf("/v2/assets/%v", s.assetId), nil, headers)
 	return
 }
