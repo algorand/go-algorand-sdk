a2aab1bc807ff02d663dbc616c9a4176d455d77c
Will Winder <wwinder.unh@gmail.com>
2021-03-19 10:34:27 -0400

    TealCompile

--- client/v2/algod/tealCompile.go
+++ client/v2/algod/tealCompile.go
@@ -7,18 +7,13 @@ import (
 	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
 )
 
-// TealCompile /v2/teal/compile
-// Given TEAL source code in plain text, return base64 encoded program bytes and
-// base32 SHA512_256 hash of program bytes (Address style).
 type TealCompile struct {
-	c      *Client
+	c *Client
+
 	source []byte
 }
 
-// Do performs HTTP request
-func (s *TealCompile) Do(ctx context.Context,
-	headers ...*common.Header) (response models.CompileResponse, err error) {
-	err = s.c.post(ctx, &response,
-		"/v2/teal/compile", s.source, headers)
+func (s *TealCompile) Do(ctx context.Context, headers ...*common.Header) (response models.CompileResponse, err error) {
+	err = s.c.get(ctx, &response, "/v2/teal/compile", nil, headers)
 	return
 }
