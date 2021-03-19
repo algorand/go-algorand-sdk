ef777a62043a07d305ad18250465e0408f010b8e
Will Winder <wwinder.unh@gmail.com>
2021-03-19 10:24:06 -0400

    wait for block

--- client/v2/algod/waitForBlock.go
+++ client/v2/algod/waitForBlock.go
@@ -3,16 +3,18 @@ package algod
 import (
 	"context"
 	"fmt"
+
 	"github.com/algorand/go-algorand-sdk/client/v2/common"
 	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
 )
 
 type StatusAfterBlock struct {
-	c     *Client
+	c *Client
+
 	round uint64
 }
 
-func (s *StatusAfterBlock) Do(ctx context.Context, headers ...*common.Header) (status models.NodeStatus, err error) {
-	err = s.c.get(ctx, &status, fmt.Sprintf("/v2/status/wait-for-block-after/%d", s.round), nil, headers)
+func (s *StatusAfterBlock) Do(ctx context.Context, headers ...*common.Header) (response models.NodeStatus, err error) {
+	err = s.c.get(ctx, &response, fmt.Sprintf("/v2/status/wait-for-block-after/%v", s.round), nil, headers)
 	return
 }
