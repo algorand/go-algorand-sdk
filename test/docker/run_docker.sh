26eff5c93fee3b0a2e5b84afbb271de5e532bf5c
Jason Paulos <jasonpaulos@users.noreply.github.com>
2021-07-09 08:47:25 -0700

    Use correct go version in CI (#228)
    * Use current Go version in Dockerfile
    
    * Add image printout
    
    * Use actual variable

--- test/docker/run_docker.sh
+++ test/docker/run_docker.sh
@@ -9,8 +9,13 @@ git clone --single-branch --branch master https://github.com/algorand/algorand-s
 #copy feature files into project
 mv test-harness/features test/features
 
+GO_VERSION=$(go version | cut -d' ' -f 3 | cut -d'.' -f 1,2)
+GO_IMAGE=golang:${GO_VERSION:2}-stretch
+
+echo "Building docker image from base \"$GO_IMAGE\""
+
 #build test environment
-docker build -t go-sdk-testing -f test/docker/Dockerfile "$(pwd)"
+docker build -t go-sdk-testing --build-arg GO_IMAGE="$GO_IMAGE" -f test/docker/Dockerfile "$(pwd)"
 
 # Start test harness environment
 ./test-harness/scripts/up.sh -p
