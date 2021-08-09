fc01f86a20fc6bbbb2bb2a55579c6b3bfb88ad03
Hang Su <hang.su@algorand.com>
2021-08-09 11:31:05 -0400

    update abi-type serialization, partly de-serialization

--- encoding/abi/abi.go
+++ encoding/abi/abi.go
@@ -1,19 +1,23 @@
 package abi
 
-import "math/big"
+import (
+	"fmt"
+	"math/big"
+	"strings"
+)
 
-// All ABI types
 /*
-uint<N>: An N-bit unsigned integer, where 8 <= N <= 512 and N % 8 = 0.
-byte // alias for uint8
-ufixed<N>x<M>
-bool
-<type>[<N>]
-address // alias for byte[32]
-<type>[]
-string
-(T1,T2,...,TN):
+   ABI-Types: uint<N>: An N-bit unsigned integer (8 <= N <= 512 and N % 8 = 0).
+            | byte (alias for uint8)
+            | ufixed <N> x <M> (8 <= N <= 512, N % 8 = 0, and 0 < M <= 160)
+            | bool
+            | address (alias for byte[32])
+            | <type> [<N>]
+            | <type> []
+            | string
+            | (T1, ..., Tn)
 */
+
 type BaseType uint32
 
 const (
@@ -41,19 +45,103 @@ type Type struct {
 	unsignedTypeSize uint16
 	// only appliable to `ufixed` precision <M>
 	unsignedTypePrecision uint16
+
+	// length for static array
+	staticArrayLength uint64
 }
 
 // String serialization
 func (t Type) String() string {
-	// TODO
+	switch t.typeFromEnum {
+	case Uint:
+		return "uint" + string(t.unsignedTypeSize)
+	case Byte:
+		return "byte"
+	case Ufixed:
+		return "ufixed" + string(t.unsignedTypeSize) + "x" + string(t.unsignedTypePrecision)
+	case Bool:
+		return "bool"
+	case ArrayStatic:
+		return "[" + string(t.staticArrayLength) + "]" + t.childTypes[0].String()
+	case Address:
+		return "address"
+	case ArrayDynamic:
+		return "[]" + t.childTypes[0].String()
+	case String:
+		return "string"
+	case Tuple:
+		typeStrings := make([]string, len(t.childTypes))
+		for i := 0; i < len(t.childTypes); i++ {
+			typeStrings[i] = t.childTypes[i].String()
+		}
+		return "(" + strings.Join(typeStrings, ", ") + ")"
+	default:
+		return "Bruh you should not be here"
+	}
 }
 
 // TypeFromstring de-serialization
 func TypeFromString(str string) (Type, error) {
-	// TODO
+	trimmedStr := strings.TrimSpace(str)
+	switch {
+	// TODO uint
+	case trimmedStr == "byte":
+		return makeByteType(), nil
+	// TODO ufixed
+	case trimmedStr == "bool":
+		return makeBoolType(), nil
+	// TODO array static
+	case trimmedStr == "address":
+		return makeAddressType(), nil
+	// TODO array dynamic
+	case trimmedStr == "string":
+		return makeStringType(), nil
+	// TODO Tuple
+	default:
+		return makeTypeDummy(),
+			fmt.Errorf("Cannot convert a string %s to an ABI type", trimmedStr)
+	}
 }
 
-// TODO makeType
+func makeTypeDummy() Type {
+	return Type{0, nil, 0, 0, 0}
+}
+
+func makeUintType(typeSize uint16) Type {
+	return Type{Uint, nil, typeSize, 0, 0}
+}
+
+func makeByteType() Type {
+	return Type{Byte, nil, 0, 0, 0}
+}
+
+func makeUFixedType(typeSize uint16, typePrecision uint16) Type {
+	return Type{Ufixed, nil, typeSize, typePrecision, 0}
+}
+
+func makeBoolType() Type {
+	return Type{Bool, nil, 0, 0, 0}
+}
+
+func makeStaticArrayType(argumentType Type, arrayLength uint64) Type {
+	return Type{ArrayStatic, []Type{argumentType}, 0, 0, arrayLength}
+}
+
+func makeAddressType() Type {
+	return Type{Address, nil, 0, 0, 0}
+}
+
+func makeDynamicType(argumentType Type) Type {
+	return Type{ArrayDynamic, []Type{argumentType}, 0, 0, 0}
+}
+
+func makeStringType() Type {
+	return Type{String, nil, 0, 0, 0}
+}
+
+func makeTupleType(argumentTypes []Type) Type {
+	return Type{Tuple, argumentTypes, 0, 0, uint64(len(argumentTypes))}
+}
 
 // Need a struct which represents an ABI value. This struct would probably
 // contain the ABI type struct and an interface{} for its value(s)
