c510fb56cfc31ae31af170fbe9c678e66ff47a1f
Hang Su <hang.su@algorand.com>
2021-08-11 14:44:10 -0400

    update abi testcases

--- encoding/abi/abi_test.go
+++ encoding/abi/abi_test.go
@@ -2,24 +2,17 @@ package abi
 
 import (
 	"fmt"
-	"reflect"
+	"github.com/stretchr/testify/require"
 	"strconv"
 	"testing"
 )
-
-func testValidFramework(funcName string, expected interface{}, actual interface{}, t *testing.T) {
-	// add equal method for struct Type
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("%s testing error: expect %s, got %s", funcName, expected, actual)
-	}
-}
 
 func TestMakeUintTypeValid(t *testing.T) {
 	for i := 8; i <= 512; i += 8 {
 		uintType, _ := MakeUintType(uint16(i))
 		expected := "uint" + strconv.Itoa(i)
 		actual := uintType.String()
-		testValidFramework("MakeUintType", expected, actual, t)
+		require.Equal(t, expected, actual, "MakeUintType: expected %s, actual %s", expected, actual)
 	}
 }
 
@@ -34,10 +27,8 @@ func TestTypeFromStringUintTypeValid(t *testing.T) {
 	for i := 8; i <= 512; i += 8 {
 		expected, _ := MakeUintType(uint16(i))
 		actual, err := TypeFromString(expected.String())
-		if err != nil {
-			t.Errorf("TypeFromString testing error: Parsing error for %s", expected.String())
-		}
-		testValidFramework("TypeFromString uint", expected, actual, t)
+		require.Equal(t, nil, err, "TypeFromString: uint parsing error: %s", expected.String())
+		require.Equal(t, expected, actual, "TypeFromString: expected %s, actual %s", expected.String(), actual.String())
 	}
 }
 
@@ -66,29 +57,13 @@ func TestTypeFromStringUintTypeInvalid(t *testing.T) {
 
 }
 
-func TestMakeByteTypeValid(t *testing.T) {
-	byteType := MakeByteType()
-	expected := byteType.String()
-	actual := "byte"
-	testValidFramework("MakeByteType", expected, actual, t)
-}
-
-func TestTypeFromStringByteTypeValid(t *testing.T) {
-	expected := MakeByteType()
-	actual, err := TypeFromString(expected.String())
-	if err != nil {
-		t.Errorf("TypeFromString testing error: Parsing error for %s", expected.String())
-	}
-	testValidFramework("TypeFromString byte", expected, actual, t)
-}
-
 func TestMakeUfixedTypeValid(t *testing.T) {
 	for i := 8; i <= 512; i += 8 {
 		for j := 1; j <= 160; j++ {
 			ufixedType, _ := MakeUFixedType(uint16(i), uint16(j))
 			expected := "ufixed" + strconv.Itoa(i) + "x" + strconv.Itoa(j)
 			actual := ufixedType.String()
-			testValidFramework("MakeUfixedType", expected, actual, t)
+			require.Equal(t, expected, actual, "TypeFromString ufixed error: expected %s, actual %s", expected, actual)
 		}
 	}
 }
@@ -98,59 +73,48 @@ func TestTypeFromStringUfixedTypeValid(t *testing.T) {
 		for j := 1; j <= 160; j++ {
 			expected, _ := MakeUFixedType(uint16(i), uint16(j))
 			actual, err := TypeFromString("ufixed" + strconv.Itoa(i) + "x" + strconv.Itoa(j))
-			if err != nil {
-				fmt.Println(err)
-				t.Errorf("TypeFromString testing error: Parsing error for %s", expected.String())
-			}
-			testValidFramework("TypeFromString ufixed", expected, actual, t)
+			require.Equal(t, nil, err, "TypeFromString ufixed parsing error: %s", expected.String())
+			require.Equal(t, expected, actual, "TypeFromString ufixed: expected %s, actual %s", expected.String(), actual.String())
 		}
 	}
 }
 
-func TestMakeAddressTypeValid(t *testing.T) {
-	addressType := MakeAddressType()
-	expected := addressType.String()
-	actual := "address"
-	testValidFramework("MakeAddressType", expected, actual, t)
-}
-
-func TestTypeFromStringAddressTypeValid(t *testing.T) {
-	expected := MakeAddressType()
-	actual, err := TypeFromString(expected.String())
-	if err != nil {
-		t.Errorf("TypeFromString testing error: Parsing error for %s", expected.String())
+func TestMakeSimpleTypeValid(t *testing.T) {
+	var testcases = []struct{
+		input Type
+		testType string
+		expected string
+	}{
+		{input: MakeBoolType(), testType: "bool", expected: "bool"},
+		{input: MakeStringType(), testType: "string", expected: "string"},
+		{input: MakeAddressType(), testType: "address", expected: "address"},
+		{input: MakeByteType(), testType: "byte", expected: "byte"},
 	}
-	testValidFramework("TypeFromString address", expected, actual, t)
-}
-
-func TestMakeStringTypeValid(t *testing.T) {
-	stringType := MakeStringType()
-	expected := stringType.String()
-	actual := "string"
-	testValidFramework("MakeStringType", expected, actual, t)
-}
-
-func TestTypeFromStringStringTypeValid(t *testing.T) {
-	expected := MakeStringType()
-	actual, err := TypeFromString(expected.String())
-	if err != nil {
-		t.Errorf("TypeFromString testing error: Parsing error for %s", expected.String())
+	for _, testcase := range testcases {
+		t.Run(fmt.Sprintf("MakeType test %s", testcase.testType), func(t *testing.T) {
+			actual := testcase.input.String()
+			require.Equal(t, testcase.expected, actual, "MakeType: expected %s, actual %s", testcase.expected, actual)
+		})
 	}
-	testValidFramework("TypeFromString string", expected, actual, t)
 }
 
-func TestMakeBoolTypeValid(t *testing.T) {
-	boolType := MakeBoolType()
-	expected := boolType.String()
-	actual := "bool"
-	testValidFramework("MakeBoolType", expected, actual, t)
-}
-
-func TestTypeFromStringBoolTypeValid(t *testing.T) {
-	expected := MakeBoolType()
-	actual, err := TypeFromString(expected.String())
-	if err != nil {
-		t.Errorf("TypeFromString testing error: Parsing error for %s", expected.String())
+func TestSimpleTypeFromStringValid(t *testing.T) {
+	var testcases = []struct {
+		input    string
+		testType string
+		expected Type
+	}{
+		{input: MakeBoolType().String(), testType: "bool", expected: MakeBoolType()},
+		{input: MakeStringType().String(), testType: "string", expected: MakeStringType()},
+		{input: MakeAddressType().String(), testType: "address", expected: MakeAddressType()},
+		{input: MakeByteType().String(), testType: "byte", expected: MakeByteType()},
 	}
-	testValidFramework("TypeFromString bool", expected, actual, t)
+	for _, testcase := range testcases {
+		t.Run(fmt.Sprintf("TypeFromString test %s", testcase.testType), func(t *testing.T) {
+			actual, err := TypeFromString(testcase.input)
+			require.Equal(t, nil, err, "TypeFromString %s parsing error", testcase.testType)
+			require.Equal(t, testcase.expected, actual, "TestFromString %s: expected %s, actual %s",
+				testcase.testType, testcase.expected.String(), actual.String())
+		})
+	}
 }
