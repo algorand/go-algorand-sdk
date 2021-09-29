4f9473dc4a51eb4f381c0447caf7ff8b31127ff8
Hang Su <hang.su@algorand.com>
2021-09-29 13:07:39 -0400

    update naming

--- encoding/abi/abi.go
+++ encoding/abi/abi.go
@@ -6,218 +6,218 @@ import (
 	"github.com/algorand/go-algorand/data/abi"
 )
 
-type ABIValue struct {
+type Value struct {
 	abiVal abi.Value
 }
 
-type ABIType struct {
+type Type struct {
 	abiType abi.Type
 }
 
-func (val ABIValue) Encode() ([]byte, error) {
+func (val Value) Encode() ([]byte, error) {
 	return val.abiVal.Encode()
 }
 
-func Decode(encoded []byte, abiType ABIType) (ABIValue, error) {
+func Decode(encoded []byte, abiType Type) (Value, error) {
 	abiVal, err := abi.Decode(encoded, abiType.abiType)
 	if err != nil {
-		return ABIValue{}, err
+		return Value{}, err
 	}
-	return ABIValue{abiVal}, nil
+	return Value{abiVal}, nil
 }
 
-func (t ABIType) String() string {
+func (t Type) String() string {
 	return t.abiType.String()
 }
 
-func TypeFromString(str string) (ABIType, error) {
+func TypeFromString(str string) (Type, error) {
 	abiTypeFromStr, err := abi.TypeFromString(str)
 	if err != nil {
-		return ABIType{}, err
+		return Type{}, err
 	}
-	return ABIType{abiTypeFromStr}, nil
+	return Type{abiTypeFromStr}, nil
 }
 
-func MakeUint8(val uint8) ABIValue {
-	return ABIValue{abi.MakeUint8(val)}
+func MakeUint8(val uint8) Value {
+	return Value{abi.MakeUint8(val)}
 }
 
-func (val ABIValue) GetUint8() (uint8, error) {
+func (val Value) GetUint8() (uint8, error) {
 	return val.abiVal.GetUint8()
 }
 
-func MakeUint16(val uint16) ABIValue {
-	return ABIValue{abi.MakeUint16(val)}
+func MakeUint16(val uint16) Value {
+	return Value{abi.MakeUint16(val)}
 }
 
-func (val ABIValue) GetUint16() (uint16, error) {
+func (val Value) GetUint16() (uint16, error) {
 	return val.abiVal.GetUint16()
 }
 
-func MakeUint32(val uint32) ABIValue {
-	return ABIValue{abi.MakeUint32(val)}
+func MakeUint32(val uint32) Value {
+	return Value{abi.MakeUint32(val)}
 }
 
-func (val ABIValue) GetUint32() (uint32, error) {
+func (val Value) GetUint32() (uint32, error) {
 	return val.abiVal.GetUint32()
 }
 
-func MakeUint64(val uint64) ABIValue {
-	return ABIValue{abi.MakeUint64(val)}
+func MakeUint64(val uint64) Value {
+	return Value{abi.MakeUint64(val)}
 }
 
-func (val ABIValue) GetUint64() (uint64, error) {
+func (val Value) GetUint64() (uint64, error) {
 	return val.abiVal.GetUint64()
 }
 
-func MakeUint(val *big.Int, size uint16) (ABIValue, error) {
+func MakeUint(val *big.Int, size uint16) (Value, error) {
 	uintV, err := abi.MakeUint(val, size)
 	if err != nil {
-		return ABIValue{}, err
+		return Value{}, err
 	}
-	return ABIValue{uintV}, nil
+	return Value{uintV}, nil
 }
 
-func (val ABIValue) GetUint() (*big.Int, error) {
+func (val Value) GetUint() (*big.Int, error) {
 	return val.abiVal.GetUint()
 }
 
-func MakeUfixed(val *big.Int, size uint16, precision uint16) (ABIValue, error) {
+func MakeUfixed(val *big.Int, size uint16, precision uint16) (Value, error) {
 	ufixedV, err := abi.MakeUfixed(val, size, precision)
 	if err != nil {
-		return ABIValue{}, err
+		return Value{}, err
 	}
-	return ABIValue{ufixedV}, nil
+	return Value{ufixedV}, nil
 }
 
-func (val ABIValue) GetUfixed() (*big.Int, error) {
+func (val Value) GetUfixed() (*big.Int, error) {
 	return val.abiVal.GetUfixed()
 }
 
-func MakeString(val string) ABIValue {
-	return ABIValue{abi.MakeString(val)}
+func MakeString(val string) Value {
+	return Value{abi.MakeString(val)}
 }
 
-func (val ABIValue) GetString() (string, error) {
+func (val Value) GetString() (string, error) {
 	return val.abiVal.GetString()
 }
 
-func MakeByte(val byte) ABIValue {
-	return ABIValue{abi.MakeByte(val)}
+func MakeByte(val byte) Value {
+	return Value{abi.MakeByte(val)}
 }
 
-func (val ABIValue) GetByte() (byte, error) {
+func (val Value) GetByte() (byte, error) {
 	return val.abiVal.GetByte()
 }
 
-func MakeAddress(val [32]byte) ABIValue {
-	return ABIValue{abi.MakeAddress(val)}
+func MakeAddress(val [32]byte) Value {
+	return Value{abi.MakeAddress(val)}
 }
 
-func (val ABIValue) GetAddress() ([32]byte, error) {
+func (val Value) GetAddress() ([32]byte, error) {
 	return val.abiVal.GetAddress()
 }
 
-func MakeBool(val bool) ABIValue {
-	return ABIValue{abi.MakeBool(val)}
+func MakeBool(val bool) Value {
+	return Value{abi.MakeBool(val)}
 }
 
-func (val ABIValue) GetBool() (bool, error) {
+func (val Value) GetBool() (bool, error) {
 	return val.abiVal.GetBool()
 }
 
-func MakeDynamicArray(values []ABIValue, elemType ABIType) (ABIValue, error) {
+func MakeDynamicArray(values []Value, elemType Type) (Value, error) {
 	tempValues := make([]abi.Value, len(values))
 	for i := 0; i < len(values); i++ {
 		tempValues[i] = values[i].abiVal
 	}
 	abiDynamicArray, err := abi.MakeDynamicArray(tempValues, elemType.abiType)
 	if err != nil {
-		return ABIValue{}, err
+		return Value{}, err
 	}
-	return ABIValue{abiDynamicArray}, nil
+	return Value{abiDynamicArray}, nil
 }
 
-func MakeStaticArray(values []ABIValue) (ABIValue, error) {
+func MakeStaticArray(values []Value) (Value, error) {
 	tempValues := make([]abi.Value, len(values))
 	for i := 0; i < len(values); i++ {
 		tempValues[i] = values[i].abiVal
 	}
 	abiStaticArray, err := abi.MakeStaticArray(tempValues)
 	if err != nil {
-		return ABIValue{}, err
+		return Value{}, err
 	}
-	return ABIValue{abiStaticArray}, nil
+	return Value{abiStaticArray}, nil
 }
 
-func MakeTuple(values []ABIValue) (ABIValue, error) {
+func MakeTuple(values []Value) (Value, error) {
 	tempValues := make([]abi.Value, len(values))
 	for i := 0; i < len(values); i++ {
 		tempValues[i] = values[i].abiVal
 	}
 	abiTuple, err := abi.MakeTuple(tempValues)
 	if err != nil {
-		return ABIValue{}, err
+		return Value{}, err
 	}
-	return ABIValue{abiTuple}, nil
+	return Value{abiTuple}, nil
 }
 
-func (val ABIValue) GetValueByIndex(index uint16) (ABIValue, error) {
+func (val Value) GetValueByIndex(index uint16) (Value, error) {
 	abiIndexVal, err := val.abiVal.GetValueByIndex(index)
 	if err != nil {
-		return ABIValue{}, err
+		return Value{}, err
 	}
-	return ABIValue{abiIndexVal}, nil
+	return Value{abiIndexVal}, nil
 }
 
-func MakeUintType(size uint16) (ABIType, error) {
+func MakeUintType(size uint16) (Type, error) {
 	abiUintT, err := abi.MakeUintType(size)
 	if err != nil {
-		return ABIType{}, err
+		return Type{}, err
 	}
-	return ABIType{abiUintT}, nil
+	return Type{abiUintT}, nil
 }
 
-func MakeUfixedType(size uint16, precision uint16) (ABIType, error) {
+func MakeUfixedType(size uint16, precision uint16) (Type, error) {
 	abiUfixedT, err := abi.MakeUfixedType(size, precision)
 	if err != nil {
-		return ABIType{}, err
+		return Type{}, err
 	}
-	return ABIType{abiUfixedT}, nil
+	return Type{abiUfixedT}, nil
 }
 
-func MakeByteType() ABIType {
-	return ABIType{abi.MakeByteType()}
+func MakeByteType() Type {
+	return Type{abi.MakeByteType()}
 }
 
-func MakeAddressType() ABIType {
-	return ABIType{abi.MakeAddressType()}
+func MakeAddressType() Type {
+	return Type{abi.MakeAddressType()}
 }
 
-func MakeStringType() ABIType {
-	return ABIType{abi.MakeStringType()}
+func MakeStringType() Type {
+	return Type{abi.MakeStringType()}
 }
 
-func MakeBoolType() ABIType {
-	return ABIType{abi.MakeBoolType()}
+func MakeBoolType() Type {
+	return Type{abi.MakeBoolType()}
 }
 
-func MakeDynamicArrayType(elemType ABIType) ABIType {
-	return ABIType{abi.MakeDynamicArrayType(elemType.abiType)}
+func MakeDynamicArrayType(elemType Type) Type {
+	return Type{abi.MakeDynamicArrayType(elemType.abiType)}
 }
 
-func MakeStaticArrayType(elemType ABIType, length uint16) ABIType {
-	return ABIType{abi.MakeStaticArrayType(elemType.abiType, length)}
+func MakeStaticArrayType(elemType Type, length uint16) Type {
+	return Type{abi.MakeStaticArrayType(elemType.abiType, length)}
 }
 
-func MakeTupleType(elemTypes []ABIType) (ABIType, error) {
+func MakeTupleType(elemTypes []Type) (Type, error) {
 	tempTypes := make([]abi.Type, len(elemTypes))
 	for i := 0; i < len(elemTypes); i++ {
 		tempTypes[i] = elemTypes[i].abiType
 	}
 	tupleT, err := abi.MakeTupleType(tempTypes)
 	if err != nil {
-		return ABIType{}, err
+		return Type{}, err
 	}
-	return ABIType{tupleT}, nil
+	return Type{tupleT}, nil
 }
