f9a45591d0167abf40d65e50c1eb64f724de7985
Hang Su <hang.su@algorand.com>
2021-10-13 18:26:12 -0400

    update abi exported interface

--- encoding/abi/abi.go
+++ encoding/abi/abi.go
@@ -1,72 +1,13 @@
 package abi
 
 import (
-	"math/big"
-
 	"github.com/algorand/go-algorand/data/abi"
 )
 
-type Value = abi.Value
 type Type = abi.Type
-
-func Decode(encoded []byte, abiType Type) (Value, error) {
-	return abi.Decode(encoded, abiType)
-}
 
 func TypeFromString(str string) (Type, error) {
 	return abi.TypeFromString(str)
-}
-
-func MakeUint8(val uint8) Value {
-	return abi.MakeUint8(val)
-}
-
-func MakeUint16(val uint16) Value {
-	return abi.MakeUint16(val)
-}
-
-func MakeUint32(val uint32) Value {
-	return abi.MakeUint32(val)
-}
-
-func MakeUint64(val uint64) Value {
-	return abi.MakeUint64(val)
-}
-
-func MakeUint(val *big.Int, size uint16) (Value, error) {
-	return abi.MakeUint(val, size)
-}
-
-func MakeUfixed(val *big.Int, size uint16, precision uint16) (Value, error) {
-	return abi.MakeUfixed(val, size, precision)
-}
-
-func MakeString(val string) Value {
-	return abi.MakeString(val)
-}
-
-func MakeByte(val byte) Value {
-	return abi.MakeByte(val)
-}
-
-func MakeAddress(val [32]byte) Value {
-	return abi.MakeAddress(val)
-}
-
-func MakeBool(val bool) Value {
-	return abi.MakeBool(val)
-}
-
-func MakeDynamicArray(values []Value, elemType Type) (Value, error) {
-	return abi.MakeDynamicArray(values, elemType)
-}
-
-func MakeStaticArray(values []Value) (Value, error) {
-	return abi.MakeStaticArray(values)
-}
-
-func MakeTuple(values []Value) (Value, error) {
-	return abi.MakeTuple(values)
 }
 
 func MakeUintType(size uint16) (Type, error) {
