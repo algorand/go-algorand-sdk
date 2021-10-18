69c4ee3902251b3216013df3c72956ab38007495
Hang Su <hang.su@algorand.com>
2021-10-18 18:11:58 -0400

    update ABI interface

--- encoding/abi/abi.go
+++ encoding/abi/abi.go
@@ -6,42 +6,6 @@ import (
 
 type Type = abi.Type
 
-func TypeFromString(str string) (Type, error) {
-	return abi.TypeFromString(str)
-}
-
-func MakeUintType(size uint16) (Type, error) {
-	return abi.MakeUintType(size)
-}
-
-func MakeUfixedType(size uint16, precision uint16) (Type, error) {
-	return abi.MakeUfixedType(size, precision)
-}
-
-func MakeByteType() Type {
-	return abi.MakeByteType()
-}
-
-func MakeAddressType() Type {
-	return abi.MakeAddressType()
-}
-
-func MakeStringType() Type {
-	return abi.MakeStringType()
-}
-
-func MakeBoolType() Type {
-	return abi.MakeBoolType()
-}
-
-func MakeDynamicArrayType(elemType Type) Type {
-	return abi.MakeDynamicArrayType(elemType)
-}
-
-func MakeStaticArrayType(elemType Type, length uint16) Type {
-	return abi.MakeStaticArrayType(elemType, length)
-}
-
-func MakeTupleType(elemTypes []Type) (Type, error) {
-	return abi.MakeTupleType(elemTypes)
+func TypeOf(str string) (Type, error) {
+	return abi.TypeOf(str)
 }
