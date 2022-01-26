dcb041d714cc84a68eed93f04f6d63b69b6ea290
algoidan <idan@algorand.com>
2022-01-26 15:51:00 +0200

    add hash type enum

--- client/v2/common/models/proof_response.go
+++ client/v2/common/models/proof_response.go
@@ -2,7 +2,9 @@ package models
 
 // ProofResponse proof of transaction in a block.
 type ProofResponse struct {
-	// Hashtype the type of hash function used to create the proof
+	// Hashtype the type of hash function used to create the proof, must be one of:
+	// * sumhash
+	// * sha512_256
 	Hashtype string `json:"hashtype,omitempty"`
 
 	// Idx index of the transaction in the block's payset.
@@ -14,7 +16,7 @@ type ProofResponse struct {
 	// Stibhash hash of SignedTxnInBlock for verifying proof.
 	Stibhash []byte `json:"stibhash"`
 
-	// Treedepth represents the depth of the tree that is being proven. the root level
-	// is not included.
+	// Treedepth represents the depth of the tree that is being proven, i.e. the number
+	// of edges from a leaf to the root.
 	Treedepth uint64 `json:"treedepth"`
 }
