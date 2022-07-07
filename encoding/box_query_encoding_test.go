dcd54a6fc4acc4a6285b0a13668c50a68f2cd0a5
michaeldiamant <michaeldiamant@users.noreply.github.com>
2022-07-07 09:58:09 -0400

    Boxes:  Add convenience methods for encoding Box names

---
+++ encoding/box_query_encoding_test.go
@@ -0,0 +1,43 @@
+package encoding
+
+import (
+	"encoding/base64"
+	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
+	"github.com/algorand/go-algorand-sdk/types"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+type example struct {
+	source           []byte
+	expectedEncoding string
+}
+
+var e = example{[]byte("tkÿÿ"), "b64:dGvDv8O/"}
+
+func TestEncode(t *testing.T) {
+	// Encode bytes
+	require.Equal(t,
+		e.expectedEncoding,
+		EncodeBytesForBoxQuery(e.source),
+	)
+
+	// Encode Box
+	b := models.Box{Name: []byte(base64.StdEncoding.EncodeToString(e.source))}
+	actual, err := EncodeBoxForBoxQuery(b)
+	require.NoError(t, err)
+	require.Equal(t,
+		e.expectedEncoding,
+		actual,
+	)
+
+	// Encode BoxReference
+	require.Equal(t,
+		e.expectedEncoding,
+		EncodeBoxReferenceForBoxQuery(types.BoxReference{Name: e.source}))
+
+	// Encode AppBoxReference
+	require.Equal(t,
+		e.expectedEncoding,
+		EncodeAppBoxReferenceForBoxQuery(types.AppBoxReference{Name: e.source}))
+}
