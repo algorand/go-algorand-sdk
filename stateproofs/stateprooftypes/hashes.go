b5db6d650c8e738f50d56c4a969388b615751172
Almog Tal <almog.tal@algorand.com>
2022-07-25 14:09:39 +0300

    CR: removed now redundant elements from hashes.go

--- stateproofs/stateprooftypes/hashes.go
+++ stateproofs/stateprooftypes/hashes.go
@@ -1,49 +1,8 @@
 package stateprooftypes
 
 import (
-	"crypto/sha256"
-	"crypto/sha512"
-	"errors"
 	"hash"
 )
-
-var (
-	ErrUnsupportedHashFuncType = errors.New("unsupported hash function type")
-)
-
-// HashType represents different hash functions
-type HashType uint16
-
-// types of hashes
-const (
-	Sha512_256 HashType = iota
-	Sumhash
-	Sha256
-	MaxHashType
-)
-
-// Sumhash512DigestSize  The size in bytes of the sumhash checksum
-const Sumhash512DigestSize = 64
-
-// MaxHashDigestSize is used to bound the max digest size. it is important to change it if a hash with
-// a longer output is introduced.
-const MaxHashDigestSize = SumhashDigestSize
-
-//size of each hash
-const (
-	Sha512_256Size    = sha512.Size256
-	SumhashDigestSize = Sumhash512DigestSize
-	Sha256Size        = sha256.Size
-)
-
-func UnmarshalHashFunc(hashStr string) (hash.Hash, error) {
-	switch hashStr {
-	case "sha256":
-		return sha256.New(), nil
-	default:
-		return nil, ErrUnsupportedHashFuncType
-	}
-}
 
 type Hashable interface {
 	ToBeHashed() (HashID, []byte)
