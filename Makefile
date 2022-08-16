dad6d1e2270791a889a0d6ce01856e18ac4ee230
Zeph Grunschlag <zeph@algorand.com>
2022-08-16 17:47:22 -0500

    Enhancement: Use Sandbox for Testing

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 SRCPATH     := $(shell pwd)
 TEST_SOURCES := $(shell cd $(SRCPATH) && go list ./...)
 TEST_SOURCES_NO_CUCUMBER := $(shell cd $(SRCPATH) && go list ./... | grep -v test)
+GO_IMAGE := golang:$(subst go,,$(shell go version | cut -d' ' -f 3 | cut -d'.' -f 1,2))-stretch
 
 lint:
 	golint `go list ./... | grep -v /vendor/`
@@ -25,7 +26,18 @@ integration:
 	go test $(TEST_SOURCES_NO_CUCUMBER)
 	cd test && go test -timeout 0s --godog.strict=true --godog.format=pretty --godog.tags="@algod,@assets,@auction,@kmd,@send,@indexer,@rekey_v1,@send.keyregtxn,@dryrun,@compile,@applications.verified,@indexer.applications,@indexer.231,@abi,@c2c,@compile.sourcemap" --test.v .
 
-docker-test:
-	./test/docker/run_docker.sh
+harness:
+	./test-harness.sh
+
+docker-gosdk-build:
+	echo "Building docker image from base $(GO_IMAGE)"
+	docker build -t go-sdk-testing --build-arg GO_IMAGE="$(GO_IMAGE)" -f test/docker/Dockerfile $(shell pwd)
+
+docker-gosdk-run:
+	docker ps -a
+	docker run -it --network host go-sdk-testing:latest
+
+docker-test: harness docker-gosdk-build docker-gosdk-run
+
 
 .PHONY: test fmt
---
+++ test-harness.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+START=$(date "+%s")
+
+THIS=$(basename "$0")
+ENV_FILE=".test-env"
+
+set -a
+source "$ENV_FILE"
+set +a
+
+rootdir=$(dirname "$0")
+pushd "$rootdir"
+
+## Reset test harness
+if [ -d "$SDK_TESTING_HARNESS" ]; then
+  pushd "$SDK_TESTING_HARNESS"
+  ./scripts/down.sh
+  popd
+  rm -rf "$SDK_TESTING_HARNESS"
+else
+  echo "$THIS: directory $SDK_TESTING_HARNESS does not exist - NOOP"
+fi
+
+git clone --depth 1 --single-branch --branch "$SDK_TESTING_BRANCH" "$SDK_TESTING_URL" "$SDK_TESTING_HARNESS"
+
+
+if [[ $OVERWRITE_TESTING_ENVIRONMENT == 1 ]]; then
+  echo "$THIS: OVERWRITE downloaded $SDK_TESTING_HARNESS/.env with $ENV_FILE:"
+  cp "$ENV_FILE" "$SDK_TESTING_HARNESS"/.env
+fi
+
+## Copy feature files into the project resources
+rm -rf test/features
+mkdir -p test/features
+cp -r "$SDK_TESTING_HARNESS"/features/* test/features
+echo "$THIS: seconds it took to get to end of cloning and copying: $(($(date "+%s") - START))s"
+
+
+## Start test harness environment
+pushd "$SDK_TESTING_HARNESS"
+./scripts/up.sh
+popd
+echo "$THIS: seconds it took to finish testing sdk's up.sh: $(($(date "+%s") - START))s"
+echo ""
+echo "--------------------------------------------------------------------------------"
+echo "|"
+echo "|    To run sandbox commands, cd into $SDK_TESTING_HARNESS/.sandbox             "
+echo "|"
+echo "--------------------------------------------------------------------------------"
