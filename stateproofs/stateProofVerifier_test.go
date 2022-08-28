10289b9feb85f1ab8e68204a74a9b1bcd07f1004
Almog Tal <almog.tal@algorand.com>
2022-08-28 15:02:29 +0300

    added unit test and resources

--- stateproofs/stateProofVerifier_test.go
+++ stateproofs/stateProofVerifier_test.go
@@ -19,28 +19,28 @@ func readJsonFile(filePath string, target interface{}, assertions *require.Asser
 	assertions.NoError(err)
 }
 
-func TestStateProofVerificationSanity(t *testing.T) {
+func TestStateProofVerification(t *testing.T) {
 	a := require.New(t)
 
-	prevStateProofFileName := "proof_20133889_to_20134144"
-	currentStateProofFileName := "proof_20134145_to_20134400"
+	prevStateProofFileName := "prevStateProof.json"
+	newStateProofFileName := "newStateProof.json"
 
-	var oldStateProofData models.StateProof
-	var newStateProofData models.StateProof
+	var prevStateProof models.StateProof
+	var newStateProof models.StateProof
 
-	readJsonFile(prevStateProofFileName, &oldStateProofData, a)
-	readJsonFile(currentStateProofFileName, &newStateProofData, a)
+	readJsonFile(prevStateProofFileName, &prevStateProof, a)
+	readJsonFile(newStateProofFileName, &newStateProof, a)
 
 	message := types.Message{
-		BlockHeadersCommitment: newStateProofData.Message.Blockheaderscommitment,
-		VotersCommitment:       newStateProofData.Message.Voterscommitment,
-		LnProvenWeight:         newStateProofData.Message.Lnprovenweight,
-		FirstAttestedRound:     newStateProofData.Message.Firstattestedround,
-		LastAttestedRound:      newStateProofData.Message.Lastattestedround,
+		BlockHeadersCommitment: newStateProof.Message.Blockheaderscommitment,
+		VotersCommitment:       newStateProof.Message.Voterscommitment,
+		LnProvenWeight:         newStateProof.Message.Lnprovenweight,
+		FirstAttestedRound:     newStateProof.Message.Firstattestedround,
+		LastAttestedRound:      newStateProof.Message.Lastattestedround,
 	}
+	encodedStateProof := types.EncodedStateProof(newStateProof.Stateproof)
 
-	encodedStateProof := types.EncodedStateProof(newStateProofData.Stateproof)
-	verifier := InitializeVerifier(oldStateProofData.Message.Voterscommitment, oldStateProofData.Message.Lnprovenweight)
+	verifier := InitializeVerifier(prevStateProof.Message.Voterscommitment, prevStateProof.Message.Lnprovenweight)
 	err := verifier.VerifyStateProofMessage(&encodedStateProof, &message)
 	a.NoError(err)
 }
