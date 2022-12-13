2d2fb97fd783d3ca5b727270679909b694765c8e
algochoi <86622919+algochoi@users.noreply.github.com>
2022-12-13 10:55:20 -0500

    Follow existing errstring conventions

--- test/applications_integration_test.go
+++ test/applications_integration_test.go
@@ -785,10 +785,8 @@ func theContentsOfTheBoxWithNameShouldBeIfThereIsAnErrorItIs(fromClient, encoded
 	} else {
 		err = fmt.Errorf("expecting algod or indexer, got " + fromClient)
 	}
-	if err != nil {
-		// If the expected error string is not empty, check if it is a substring of the actual error string.
-		// Note that if the expected error string is empty, then the second condition will always return true.
-		if errStr != "" && strings.Contains(err.Error(), errStr) {
+	if err != nil && len(errStr) != 0 {
+		if strings.Contains(err.Error(), errStr) {
 			return nil
 		}
 		return err
