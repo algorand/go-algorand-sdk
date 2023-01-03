a935089a4bb5e07ca223a17f28114e179be94243
shiqizng <80276844+shiqizng@users.noreply.github.com>
2023-01-03 17:24:27 -0500

    enhancement: add genesis type (#443)
    * adding genesis type

---
+++ types/genesis.go
@@ -0,0 +1,126 @@
+package types
+
+import (
+	"crypto/sha512"
+	"fmt"
+
+	"github.com/algorand/go-algorand-sdk/v2/encoding/msgpack"
+)
+
+// GenesisHashID is the Genesis HashID defined in go-algorand/protocol/hash.go
+var GenesisHashID = "GE"
+
+// A Genesis object defines an Algorand "universe"
+type Genesis struct {
+	_struct struct{} `codec:",omitempty,omitemptyarray"`
+
+	// The SchemaID allows nodes to store data specific to a particular
+	// universe
+	SchemaID string `codec:"id"`
+
+	// Network identifies the unique algorand network for which the ledger
+	// is valid.
+	Network string `codec:"network"`
+
+	// Proto is the consensus protocol in use at the genesis block.
+	Proto string `codec:"proto"`
+
+	// Allocation determines the initial accounts and their state.
+	Allocation []GenesisAllocation `codec:"alloc"`
+
+	// RewardsPool is the address of the rewards pool.
+	RewardsPool string `codec:"rwd"`
+
+	// FeeSink is the address of the fee sink.
+	FeeSink string `codec:"fees"`
+
+	// Timestamp for the genesis block
+	Timestamp int64 `codec:"timestamp"`
+
+	// Arbitrary genesis comment string - will be excluded from file if empty
+	Comment string `codec:"comment"`
+
+	// DevMode defines whether this network operates in a developer mode or not. Developer mode networks
+	// are a single node network, that operates without the agreement service being active. In liue of the
+	// agreement service, a new block is generated each time a node receives a transaction group. The
+	// default value for this field is "false", which makes this field empty from it's encoding, and
+	// therefore backward compatible.
+	DevMode bool `codec:"devmode"`
+}
+
+type GenesisAllocation struct {
+	_struct struct{} `codec:""`
+
+	Address string  `codec:"addr"`
+	Comment string  `codec:"comment"`
+	State   Account `codec:"state"`
+}
+
+type Account struct {
+	_struct struct{} `codec:",omitempty,omitemptyarray"`
+
+	Status          byte     `codec:"onl"`
+	MicroAlgos      uint64   `codec:"algo"`
+	VoteID          [32]byte `codec:"vote"`
+	SelectionID     [32]byte `codec:"sel"`
+	StateProofID    [64]byte `codec:"stprf"`
+	VoteLastValid   uint64   `codec:"voteLst"`
+	VoteKeyDilution uint64   `codec:"voteKD"`
+}
+
+// ID is the effective Genesis identifier - the combination
+// of the network and the ledger schema version
+func (genesis Genesis) ID() string {
+	return string(genesis.Network) + "-" + genesis.SchemaID
+}
+
+// Hash is the genesis hash.
+func (genesis Genesis) Hash() Digest {
+	hashRep := []byte(GenesisHashID)
+	data := msgpack.Encode(genesis)
+	hashRep = append(hashRep, data...)
+	return sha512.Sum512_256(hashRep)
+}
+
+// GenesisBalances contains the information needed to generate a new ledger
+type GenesisBalances struct {
+	Balances    map[Address]Account
+	FeeSink     Address
+	RewardsPool Address
+	Timestamp   int64
+}
+
+// Balances returns the genesis account balances.
+func (genesis Genesis) Balances() (GenesisBalances, error) {
+	genalloc := make(map[Address]Account)
+	for _, entry := range genesis.Allocation {
+		addr, err := DecodeAddress(entry.Address)
+		if err != nil {
+			return GenesisBalances{}, fmt.Errorf("cannot parse genesis addr %s: %w", entry.Address, err)
+		}
+
+		_, present := genalloc[addr]
+		if present {
+			return GenesisBalances{}, fmt.Errorf("repeated allocation to %s", entry.Address)
+		}
+
+		genalloc[addr] = entry.State
+	}
+
+	feeSink, err := DecodeAddress(genesis.FeeSink)
+	if err != nil {
+		return GenesisBalances{}, fmt.Errorf("cannot parse fee sink addr %s: %w", genesis.FeeSink, err)
+	}
+
+	rewardsPool, err := DecodeAddress(genesis.RewardsPool)
+	if err != nil {
+		return GenesisBalances{}, fmt.Errorf("cannot parse rewards pool addr %s: %w", genesis.RewardsPool, err)
+	}
+
+	return MakeTimestampedGenesisBalances(genalloc, feeSink, rewardsPool, genesis.Timestamp), nil
+}
+
+// MakeTimestampedGenesisBalances returns the information needed to bootstrap the ledger based on a given time
+func MakeTimestampedGenesisBalances(balances map[Address]Account, feeSink, rewardsPool Address, timestamp int64) GenesisBalances {
+	return GenesisBalances{Balances: balances, FeeSink: feeSink, RewardsPool: rewardsPool, Timestamp: timestamp}
+}
---
+++ types/genesis_test.go
@@ -0,0 +1,155 @@
+package types
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/algorand/go-algorand-sdk/v2/encoding/json"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEncodeDecodeGenesis(t *testing.T) {
+	genesisStr, err := ioutil.ReadFile("test_resource/mainnet_genesis.json")
+	require.NoError(t, err)
+
+	var genesis Genesis
+	err = json.Decode(genesisStr, &genesis)
+	require.NoError(t, err)
+	assert.Equal(t, base64.StdEncoding.EncodeToString(json.Encode(genesis)), base64.StdEncoding.EncodeToString(genesisStr))
+	// hash value taken from https://developer.algorand.org/docs/get-details/algorand-networks/mainnet/#genesis-hash
+	expectedHash := "wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8="
+	gh := genesis.Hash()
+	b := gh[:]
+	assert.Equal(t, base64.StdEncoding.EncodeToString(b), expectedHash)
+}
+
+func TestGenesis_Balances(t *testing.T) {
+	containsErrorFunc := func(str string) assert.ErrorAssertionFunc {
+		return func(_ assert.TestingT, err error, i ...interface{}) bool {
+			require.ErrorContains(t, err, str)
+			return true
+		}
+	}
+	mustAddr := func(addr string) Address {
+		address, err := DecodeAddress(addr)
+		require.NoError(t, err)
+		return address
+	}
+	makeAddr := func(addr uint64) Address {
+		var address Address
+		address[0] = byte(addr)
+		return address
+	}
+	acctWith := func(algos uint64, addr string) GenesisAllocation {
+		return GenesisAllocation{
+			_struct: struct{}{},
+			Address: addr,
+			Comment: "",
+			State: Account{
+				MicroAlgos: algos,
+			},
+		}
+	}
+	goodAddr := makeAddr(100)
+	allocation1 := acctWith(1000, makeAddr(1).String())
+	allocation2 := acctWith(2000, makeAddr(2).String())
+	badAllocation := acctWith(1234, "El Toro Loco")
+	type fields struct {
+		Allocation  []GenesisAllocation
+		FeeSink     string
+		RewardsPool string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    GenesisBalances
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "basic test",
+			fields: fields{
+				Allocation:  []GenesisAllocation{allocation1},
+				FeeSink:     goodAddr.String(),
+				RewardsPool: goodAddr.String(),
+			},
+			want: GenesisBalances{
+				Balances: map[Address]Account{
+					mustAddr(allocation1.Address): allocation1.State,
+				},
+				FeeSink:     goodAddr,
+				RewardsPool: goodAddr,
+				Timestamp:   0,
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "two test",
+			fields: fields{
+				Allocation:  []GenesisAllocation{allocation1, allocation2},
+				FeeSink:     goodAddr.String(),
+				RewardsPool: goodAddr.String(),
+			},
+			want: GenesisBalances{
+				Balances: map[Address]Account{
+					mustAddr(allocation1.Address): allocation1.State,
+					mustAddr(allocation2.Address): allocation2.State,
+				},
+				FeeSink:     goodAddr,
+				RewardsPool: goodAddr,
+				Timestamp:   0,
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "bad fee sink",
+			fields: fields{
+				Allocation:  []GenesisAllocation{allocation1, allocation2},
+				RewardsPool: goodAddr.String(),
+			},
+			wantErr: containsErrorFunc("cannot parse fee sink addr"),
+		},
+		{
+			name: "bad rewards pool",
+			fields: fields{
+				Allocation: []GenesisAllocation{allocation1, allocation2},
+				FeeSink:    goodAddr.String(),
+			},
+			wantErr: containsErrorFunc("cannot parse rewards pool addr"),
+		},
+		{
+			name: "bad genesis addr",
+			fields: fields{
+				Allocation:  []GenesisAllocation{badAllocation},
+				FeeSink:     goodAddr.String(),
+				RewardsPool: goodAddr.String(),
+			},
+			wantErr: containsErrorFunc("cannot parse genesis addr"),
+		},
+		{
+			name: "repeat address",
+			fields: fields{
+				Allocation:  []GenesisAllocation{allocation1, allocation1},
+				FeeSink:     goodAddr.String(),
+				RewardsPool: goodAddr.String(),
+			},
+			wantErr: containsErrorFunc("repeated allocation to"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			genesis := Genesis{
+				Allocation:  tt.fields.Allocation,
+				FeeSink:     tt.fields.FeeSink,
+				RewardsPool: tt.fields.RewardsPool,
+			}
+			got, err := genesis.Balances()
+			if tt.wantErr(t, err, fmt.Sprintf("Balances()")) {
+				return
+			}
+			assert.Equalf(t, tt.want, got, "Balances()")
+		})
+	}
+}
