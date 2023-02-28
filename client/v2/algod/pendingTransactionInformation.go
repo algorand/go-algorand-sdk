acaa2d4525eb788c9d2c1c1f7c0714b4c73627b9
Will Winder <wwinder.unh@gmail.com>
2023-02-28 14:52:01 -0500

    regenerate SDK

--- client/v2/algod/pendingTransactionInformation.go
+++ client/v2/algod/pendingTransactionInformation.go
@@ -12,7 +12,8 @@ import (
 // PendingTransactionInformationParams contains all of the query parameters for url serialization.
 type PendingTransactionInformationParams struct {
 
-	// Format configures whether the response object is JSON or MessagePack encoded.
+	// Format configures whether the response object is JSON or MessagePack encoded. If
+	// not provided, defaults to JSON.
 	Format string `url:"format,omitempty"`
 }
 
