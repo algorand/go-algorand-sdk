436e341b149ad9c4c911747553c0443ab91990d8
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-03-10 13:50:57 -0500

    Adding codec and debug examples

--- _examples/debug.go
+++ _examples/debug.go
@@ -1,4 +1,81 @@
 package main
 
-// Missing DEBUG_DRYRUN_DUMP in GOSDK examples (in ../docs/get-details/dapps/smart-contracts/debugging.md:127)
-// Missing DEBUG_DRYRUN_SUBMIT in GOSDK examples (in ../docs/get-details/dapps/smart-contracts/debugging.md:299)
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/algorand/go-algorand-sdk/v2/crypto"
+	"github.com/algorand/go-algorand-sdk/v2/encoding/msgpack"
+	"github.com/algorand/go-algorand-sdk/v2/transaction"
+	"github.com/algorand/go-algorand-sdk/v2/types"
+)
+
+func main() {
+
+	algodClient := getAlgodClient()
+	accts, err := getSandboxAccounts()
+	if err != nil {
+		log.Fatalf("failed to get sandbox accounts: %s", err)
+	}
+
+	acct1 := accts[0]
+
+	appID := deployApp(algodClient, acct1)
+
+	// example: DEBUG_DRYRUN_DUMP
+	var (
+		args     [][]byte
+		accounts []string
+		apps     []uint64
+		assets   []uint64
+	)
+
+	sp, err := algodClient.SuggestedParams().Do(context.Background())
+	if err != nil {
+		log.Fatalf("failed to get suggested params: %s", err)
+	}
+
+	appCallTxn, err := transaction.MakeApplicationNoOpTx(
+		appID, args, accounts, apps, assets, sp, acct1.Address,
+		nil, types.Digest{}, [32]byte{}, types.Address{},
+	)
+	if err != nil {
+		log.Fatalf("Failed to create app call txn: %+v", err)
+	}
+
+	_, stxn, err := crypto.SignTransaction(acct1.PrivateKey, appCallTxn)
+	if err != nil {
+		log.Fatalf("Failed to sign app txn: %+v", err)
+	}
+
+	signedAppCallTxn := types.SignedTxn{}
+	msgpack.Decode(stxn, &signedAppCallTxn)
+
+	drr, err := transaction.CreateDryrun(algodClient, []types.SignedTxn{signedAppCallTxn}, nil, context.Background())
+	if err != nil {
+		log.Fatalf("Failed to create dryrun: %+v", err)
+	}
+
+	os.WriteFile("dryrun.msgp", msgpack.Encode(drr), 0666)
+	// example: DEBUG_DRYRUN_DUMP
+
+	// example: DEBUG_DRYRUN_SUBMIT
+	// Create the dryrun request object
+	drReq, err := transaction.CreateDryrun(algodClient, []types.SignedTxn{signedAppCallTxn}, nil, context.Background())
+	if err != nil {
+		log.Fatalf("Failed to create dryrun: %+v", err)
+	}
+
+	// Pass dryrun request to algod server
+	dryrunResponse, err := algodClient.TealDryrun(drReq).Do(context.Background())
+	if err != nil {
+		log.Fatalf("failed to dryrun request: %s", err)
+	}
+
+	// Inspect the response to check result
+	for _, txn := range dryrunResponse.Txns {
+		log.Printf("%+v", txn.AppCallTrace)
+	}
+	// example: DEBUG_DRYRUN_SUBMIT
+}
--- _examples/util.go
+++ _examples/util.go
@@ -11,6 +11,8 @@ import (
 	"github.com/algorand/go-algorand-sdk/v2/client/kmd"
 	"github.com/algorand/go-algorand-sdk/v2/client/v2/algod"
 	"github.com/algorand/go-algorand-sdk/v2/crypto"
+	"github.com/algorand/go-algorand-sdk/v2/transaction"
+	"github.com/algorand/go-algorand-sdk/v2/types"
 )
 
 // add sandbox and other stuff
@@ -103,4 +105,78 @@ func compileTeal(algodClient *algod.Client, path string) []byte {
 		log.Fatalf("failed to decode compiled program: %s", err)
 	}
 	return bin
+}
+
+func deployApp(algodClient *algod.Client, creator crypto.Account) uint64 {
+
+	var (
+		approvalBinary = make([]byte, 1000)
+		clearBinary    = make([]byte, 1000)
+	)
+
+	// Compile approval program
+	approvalTeal, err := ioutil.ReadFile("calculator/approval.teal")
+	if err != nil {
+		log.Fatalf("failed to read approval program: %s", err)
+	}
+
+	approvalResult, err := algodClient.TealCompile(approvalTeal).Do(context.Background())
+	if err != nil {
+		log.Fatalf("failed to compile program: %s", err)
+	}
+
+	_, err = base64.StdEncoding.Decode(approvalBinary, []byte(approvalResult.Result))
+	if err != nil {
+		log.Fatalf("failed to decode compiled program: %s", err)
+	}
+
+	// Compile clear program
+	clearTeal, err := ioutil.ReadFile("calculator/clear.teal")
+	if err != nil {
+		log.Fatalf("failed to read clear program: %s", err)
+	}
+
+	clearResult, err := algodClient.TealCompile(clearTeal).Do(context.Background())
+	if err != nil {
+		log.Fatalf("failed to compile program: %s", err)
+	}
+
+	_, err = base64.StdEncoding.Decode(clearBinary, []byte(clearResult.Result))
+	if err != nil {
+		log.Fatalf("failed to decode compiled program: %s", err)
+	}
+
+	// Create application
+	sp, err := algodClient.SuggestedParams().Do(context.Background())
+	if err != nil {
+		log.Fatalf("error getting suggested tx params: %s", err)
+	}
+
+	txn, err := transaction.MakeApplicationCreateTx(
+		false, approvalBinary, clearBinary,
+		types.StateSchema{}, types.StateSchema{},
+		nil, nil, nil, nil,
+		sp, creator.Address, nil,
+		types.Digest{}, [32]byte{}, types.ZeroAddress,
+	)
+	if err != nil {
+		log.Fatalf("failed to make txn: %s", err)
+	}
+
+	txid, stx, err := crypto.SignTransaction(creator.PrivateKey, txn)
+	if err != nil {
+		log.Fatalf("failed to sign transaction: %s", err)
+	}
+
+	_, err = algodClient.SendRawTransaction(stx).Do(context.Background())
+	if err != nil {
+		log.Fatalf("failed to send transaction: %s", err)
+	}
+
+	confirmedTxn, err := transaction.WaitForConfirmation(algodClient, txid, 4, context.Background())
+	if err != nil {
+		log.Fatalf("error waiting for confirmation:  %s", err)
+	}
+
+	return confirmedTxn.ApplicationIndex
 }
