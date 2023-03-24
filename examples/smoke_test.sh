3cc43e63702ebd9891ff8278898b0f29be2da284
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-03-24 10:08:45 -0400

    replace bash shebang

--- examples/smoke_test.sh
+++ examples/smoke_test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/env/bin bash
 
 export ALGOD_PORT="60000"
 export INDEXER_PORT="59999"
@@ -12,16 +12,3 @@ for dir in */; do
     go run "${dir}main.go"
   fi
 done
-
-#go run account/main.go
-#go run apps/main.go
-#go run asa/main.go
-#go run atc/main.go
-#go run atomic_transactions/main.go
-#go run codec/main.go
-#go run debug/main.go
-#go run indexer/main.go
-#go run kmd/main.go
-#go run lsig/main.go
-#go run participation/main.go
-#go run overview/main.go
