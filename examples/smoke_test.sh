01a3f032ef31742fb5f510b703564ec28feaf902
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-03-24 09:25:10 -0400

    test all directories containing a main.go file

--- examples/smoke_test.sh
+++ examples/smoke_test.sh
@@ -4,15 +4,24 @@ export ALGOD_PORT="60000"
 export INDEXER_PORT="59999"
 export KMD_PORT="60001"
 
-go run account/main.go
-go run apps/main.go
-go run asa/main.go
-go run atc/main.go
-go run atomic_transactions/main.go
-go run codec/main.go
-go run debug/main.go
-go run indexer/main.go
-go run kmd/main.go
-go run lsig/main.go
-go run participation/main.go
-go run overview/main.go
+# Loop through each directory in the current working directory
+for dir in */; do
+  # Check if main.go exists in the directory
+  if [ -f "${dir}main.go" ]; then
+    # Run the "go run" command with the relative path
+    go run "${dir}main.go"
+  fi
+done
+
+#go run account/main.go
+#go run apps/main.go
+#go run asa/main.go
+#go run atc/main.go
+#go run atomic_transactions/main.go
+#go run codec/main.go
+#go run debug/main.go
+#go run indexer/main.go
+#go run kmd/main.go
+#go run lsig/main.go
+#go run participation/main.go
+#go run overview/main.go
