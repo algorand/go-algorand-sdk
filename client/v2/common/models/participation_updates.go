6a8662eae8b6efcb0a9016014546aaf4e29180fa
John Jannotti <jannotti@gmail.com>
2024-04-24 10:29:44 -0400

    Not yet, wait for indexer

--- client/v2/common/models/participation_updates.go
+++ client/v2/common/models/participation_updates.go
@@ -6,8 +6,4 @@ type ParticipationUpdates struct {
 	// ExpiredParticipationAccounts (partupdrmv) a list of online accounts that needs
 	// to be converted to offline since their participation key expired.
 	ExpiredParticipationAccounts []string `json:"expired-participation-accounts,omitempty"`
-
-	// AbsentParticipationAccounts contains a list of online accounts that
-	// needs to be converted to offline since they are not proposing.
-	AbsentParticipationAccounts []string `json:"absent-particpation-accounts,omitempty"`
 }
